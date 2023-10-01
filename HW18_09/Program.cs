5b565ffa784659e595b17b9ea960b7b0a55f410c
Elizabeth2803 <elizaart2801@gmail.com>
2023-10-01 22:08:33 +0200

    Добавьте файлы проекта.

---
+++ HW18_09/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Первая задача..
+Console.WriteLine("Задача первая:");
+int Petya = 10;
+int Alesha = Petya - 4;
+int Vanya = Alesha + 2;
+Console.WriteLine($"Петя взял {Petya} книг, Алеша взял {Alesha} книг, Ваня взял {Vanya} книг");
+int res = Petya + Alesha + Vanya;
+Console.WriteLine($"Они взяли вместе {res} книг");
+Console.WriteLine("                            ");
+
+//Вторая задача.
+Console.WriteLine("                            ");
+Console.WriteLine("Задача вторая:");
+int firstBox = 23;
+int secondBox = firstBox - 11;
+int resSum= firstBox + secondBox;
+Console.WriteLine($"В обеих корзинах {resSum} яблок");
